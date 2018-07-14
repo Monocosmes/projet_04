80e19ffe29c2c6a8898d04138643e591046261dc
Cro Magnon <loupssolitaires@gmail.com>
2018-07-14 11:18:35 +0200

    Create config file _config.php. It contains the autoload

---
+++ _config.php
@@ -0,0 +1,44 @@
+<?php
+
+class MyAutoload
+{
+    public static function start()
+    {
+    	spl_autoload_register(array(__CLASS__, 'autoload'));
+
+    	$root = $_SERVER['DOCUMENT_ROOT'];
+		$host = $_SERVER['HTTP_HOST'];
+		$siteName = '/projet_04/';
+		
+		define('HOST', 'http://'.$host.$siteName);
+		define('ROOT', $root.$siteName);
+		
+		define('CONTROLLERFRONT', ROOT.'controller/frontend/');
+		define('CONTROLLERBACK', ROOT.'controller/backend/');
+		define('VIEW', ROOT.'view/');
+		define('MODEL', ROOT.'model/');
+		define('CLASSES', ROOT.'classes/');
+		
+		define('ASSETS', HOST.'assets/');
+    }
+
+    public static function autoload($class)
+    {
+    	if(file_exists(MODEL.$class.'.php'))
+    	{
+    		require_once MODEL.$class.'.php';
+    	}
+    	else if(file_exists(CLASSES.$class.'.php'))
+    	{
+    		require_once CLASSES.$class.'.php';
+    	}
+    	else if(file_exists(CONTROLLERFRONT.$class.'.php'))
+    	{
+    		require_once CONTROLLERFRONT.$class.'.php';
+    	}
+    	else if(file_exists(CONTROLLERBACK.$class.'.php'))
+    	{
+    		require_once CONTROLLERBACK.$class.'.php';
+    	}
+    }
+}
