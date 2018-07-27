600761923c46c24a952f9ceb339fc40e72a589dc
Cro Magnon <loupssolitaires@gmail.com>
2018-07-27 15:26:41 +0200

    Updated for mobile devices

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -20,7 +20,7 @@ class Popup
 
 	deleteAccount(e)
 	{
-		$('body').append('<div id="popupContainer"><div id="popup"><div id="frame"><h2>Supprimer mon compte</h2><p>En effaçant votre compte, vous renoncez à votre droit de modifier ou effacer les messages que vous avez publié sur ce site<br />Souhaitez-vous continuer ?</p><div class="displayFlex center"><a class="button" href="'+this.host+'deleteAccount/userId/'+e.target.value+'">Confirmer</a><button class="button" id="cancelDelete">Annuler</button></div></div></div></div>');
+		$('body').append('<div id="popupContainer"><div id="popup"><div class="mainBorder" id="frame"><h2 class="uppercase">Supprimer mon compte</h2><p>En effaçant votre compte, vous renoncez à votre droit de modifier ou effacer les messages que vous avez publié sur ce site<br />Souhaitez-vous continuer ?</p><div class="displayFlex center"><a class="button" href="'+this.host+'deleteAccount/userId/'+e.target.value+'">Confirmer</a><button class="button" id="cancelDelete">Annuler</button></div></div></div></div>');
 		$('#cancelDelete').click(() => {this.closeWindow()});
 		$('#popupContainer').height(this.documentHeight);
 	}
@@ -29,7 +29,7 @@ class Popup
 	{
 		e.preventDefault();
 
-		$('body').append('<div id="popupContainer"><div id="popup"><div id="frame"><h2>Connexion</h2><form method="post" action="'+this.host+'signin"><label for="login">Identifiant / Email</label><input type="text" name="login" id="login" /><label for="password">Mot de passe</label><input type="password" name="password" id="password" /><div class="displayFlex center"><button type="submit" class="button">Confirmer</button><button class="button" id="cancelSignin">Annuler</button></div></form></div></div></div>');
+		$('body').append('<div id="popupContainer"><div id="popup"><div class="mainBorder" id="frame"><h2 class="uppercase">Connexion</h2><form method="post" action="'+this.host+'signin"><label for="login">Identifiant / Email</label><input type="text" name="login" id="login" /><label for="password">Mot de passe</label><input type="password" name="password" id="password" /><div class="displayFlex center"><button type="submit" class="button">Confirmer</button><button class="button" id="cancelSignin">Annuler</button></div></form></div></div></div>');
 		$('#cancelSignin').click(() => {this.closeWindow()});
 		$('#popupContainer').height(this.documentHeight);
 	}
